889da16fe778bfce6bdac801b7497c6b7be74782
d0odLe <cryted@163.com>
2023-09-01 15:47:19 +0800

    add TestController add func1

--- src/main/java/com/example/testgit/TestController.java
+++ src/main/java/com/example/testgit/TestController.java
@@ -1,8 +1,17 @@
 package com.example.testgit;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/test")
 public class TestController {
 
+    @GetMapping("/func1")
+    @ResponseBody
+    public String func1(){
+        return "func1";
+    }
 }
